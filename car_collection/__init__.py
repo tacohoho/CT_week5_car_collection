bfa1c674f955d65a03da7e8e037a04cccd40d42d
tacohoho <taeho0000@gmail.com>
2021-10-20 09:45:39 -0500

    Connected to db

--- car_collection/__init__.py
+++ car_collection/__init__.py
@@ -1,6 +1,17 @@
 from flask import Flask
+from config import Config
 from .site.routes import site
+from .authentication.routes import auth
+from flask_migrate import Migrate
+from .models import db
+
 #instantiating a new flask app
 app = Flask(__name__)
+app.config.from_object(Config)
 
 app.register_blueprint(site)
+app.register_blueprint(auth)
+
+db.init_app(app)
+
+migrate = Migrate(app, db)
--- config.py
+++ config.py
@@ -1,3 +1,10 @@
 import os
 
+from flask_sqlalchemy import SQLAlchemy
+
 basedir = os.path.abspath(os.path.dirname(__file__))
+
+class Config:
+    SECRET_KEY = 'You will never guess...'
+    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://postgres:9248jong@localhost:5432/car_collection'
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
